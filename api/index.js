ff45719caa8c88b7c4c514d663ec73508aa105c3
NinadChaugule <ninadchaugule123@gmail.com>
2024-06-12 17:56:00 +0530

    Created user model

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+
 mongoose.connect(process.env.MONGO).then(()=> {
-    console.log("Connected to the server");
-}).catch((err)=>{
+    console.log("Server is connected brooo");
+})
+.catch((err)=>{
     console.log(err);
 });
-
 
 const app=express();
 

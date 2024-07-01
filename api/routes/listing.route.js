90719bc7d21c6bc226069cc7d013054ad21cfd26
NinadChaugule <ninadchaugule123@gmail.com>
2024-07-01 19:35:26 +0530

    Completed listing page and added slider to listing page

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 import  express from "express";
-import { createListing } from "../controller.js/listing.control.js";
+import { createListing, deleteListing, updateListing, getListing} from "../controller.js/listing.control.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
-const router = express.Router();
+const router = express.Router(); 
 
 router.post('/create',verifyToken ,createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id',getListing);
+
 
 export default router;
